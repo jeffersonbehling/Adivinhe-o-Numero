49c41351bfd4f1705e537dce0d2baff2f23e9573
Jefferson Vantuir <jefferson@localhost.localdomain>
2016-10-14 16:54:38 -0300

    Ajuste do Script Java

--- Java/adivinharNumero.java
+++ Java/adivinharNumero.java
@@ -6,14 +6,26 @@ public class adivinharNumero {
 
 		boolean acertou = false;
 		int numSorteado = (int) (Math.random() * 10);
+		int numTentativas = 0;
 
 		while(acertou == false){
+			if(numTentativas == 5){
+				System.out.println("Você perdeu!!!");
+				acertou = true;
+			}
+			System.out.println("Número de Tentativas: "+ numTentativas);
 			System.out.println("Digite um número: ");
 			int numero = entrada.nextInt();
 			if(numero == numSorteado){
 				System.out.println("PARABÉNS!!! Você acertou o número. O número era "+ numSorteado);
 				acertou = true;
 			}else{
+				numTentativas++;
+				if(numSorteado < numero){
+					System.out.println("\nO número é menor.\n");
+				}else{
+					System.out.println("\nO número é maior.\n");
+				}
 				System.out.println("Errou!!! Tente novamente.\n\n");
 			}
 		}
